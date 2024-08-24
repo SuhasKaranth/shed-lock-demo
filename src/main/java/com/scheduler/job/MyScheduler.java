37e541dc43a666275b5fd980d222fc5f40c4561a
Suhas Karanth <suhas.karanth@gmail.com>
2024-08-24 09:36:14 +0400

    Initial commit

--- src/main/java/com/scheduler/job/MyScheduler.java
+++ src/main/java/com/scheduler/job/MyScheduler.java
@@ -6,22 +6,51 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
 public class MyScheduler {
 
-    @Scheduled(fixedDelay = 1000)
-    @SchedulerLock(name = "scheduledTaskName1" ,lockAtMostFor = "5m" ,lockAtLeastFor = "1s")
+    @Autowired
+    private SmsRecordRepository smsRecordRepository;
+
+
+    @Scheduled(fixedDelay = 5000)
+    @SchedulerLock(name = "scheduledSMSFetch", lockAtMostFor = "5m", lockAtLeastFor = "4s")
     public void myJob() throws InterruptedException {
         LockAssert.assertLocked();
 
-        long round = Math.round(Math.random() * 100);
-        log.info("###### Job starting {} tasks ###### ", round);
+        // Fetch the top 10 records with null status
+        List<SmsRecord> smsRecords = smsRecordRepository.findTop10ByStatusIsNull();
 
-        for (int i = 0; i < round; i++){
-            long wait = Math.round(Math.random() * 100);
-            log.info("processing time "+ wait);
-            Thread.sleep(wait);
+        // Process the records (logging for demo purposes)
+        if (!smsRecords.isEmpty()) {
+            smsRecords.forEach(record -> System.out.println("Fetched SMS Record with null status :" + record));
+            // Extract IDs from the fetched records
+            List<String> ids = smsRecords.stream()
+                    .map(SmsRecord::getId)
+                    .collect(Collectors.toList());
+
+
+            // Update the status of these records to "Processing"
+            smsRecordRepository.updateStatusToProcessing(ids, "Processing");
+
+            // Log the update
+            System.out.println("Updated status of " + ids.size() + " records to 'Processing'.");
+
+            Thread.sleep(3000);
+
+            smsRecordRepository.updateStatusToProcessing(ids, "Processed");
+
+            // Log the update
+            System.out.println("Updated status of " + ids.size() + " records to 'Processed'.");
+        } else {
+            System.out.println("No SMS records found with null status!");
         }
     }
 }
+
